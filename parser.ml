0cd88f2c86e15e8d21db4f346654f4120371a824
JeanGalte <wontusemailcom@protonmail.com>
2023-08-29 18:53:55 +0200

    Better parenthesis management

--- parser.ml
+++ parser.ml
@@ -1,6 +1,6 @@
-open Char
+(* open Char
 open List
-
+ *)
 exception Process_Err of string
 exception Parse_Err of string
 
@@ -67,14 +67,15 @@ let merge (left : lam_not_built) (right : lam_not_built) : lam_not_built =
 Operating on strings, or char list
 *)
 
-(* splits a list from the very next ) *)
+(* splits an expression with the corresponding parenthesis ) *)
 let split_par (l : char list) : (char list * char list)  = 
-	let rec aux (l : char list) (acc : char list) : (char list * char list) =
+	let rec aux (l : char list) (acc : char list) (n : int) : (char list * char list) =
 		match l with
-		| x :: xs when x = ')' -> (List.rev acc, xs)
-		| x :: xs -> aux xs (x :: acc) 
+		| x :: xs when x = ')'  -> if n = 0 then (List.rev acc, xs) else aux xs (x :: acc) (pred n)
+		| x :: xs when x = '(' -> aux xs (x :: acc) (succ n)		
+		| x :: xs -> aux xs (x :: acc) n
 		| [] -> raise (Parse_Err "Missing closing parenthesis")
-	in aux l []
+	in aux l [] 0
 
 (* Exploding the original string to a char list *)
 let explode (s : string) : char list =
@@ -86,7 +87,7 @@ let explode (s : string) : char list =
 let lambda_well_written (s : char list) : (char * bool) = 
 	match s with
 	| x :: y :: z :: xs -> 
-		(x, (List.mem x (explode "abcdefghijklmnopqrstuvwxyz")) && y = '.' && (List.mem z (explode "abcdefghijklmnopqrstuvwxyz\\")))  
+		(x, (List.mem x (explode "abcdefghijklmnopqrstuvwxyz")) && y = '.' && (List.mem z (explode "abcdefghijklmnopqrstuvwxyz(\\")))  
 	| _ -> raise (Parse_Err "The lambda term is not well written, any λ must be followed by a letter, a dot and then by a letter. Here a λ is followed by less than 2 chars.")
 
 let print_test (s : char list) : unit list = List.map print_char s
