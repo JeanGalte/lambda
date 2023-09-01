68302655c8bf9818d81518c62cda928d578c1c39
JeanGalte <wontusemailcom@protonmail.com>
2023-09-01 10:09:54 +0200

    better identifier + able to add new terms

--- identifier.ml
+++ identifier.ml
@@ -1,10 +1,13 @@
-(* open Lambda
+(* 
+open Lambda
 open Char
 open String_m
  *)
 (* 
 To be parsed properly, identified terms must start with an uppercase letter. For example, Omega = (\x.xx)(\x.xx)
 *)
+
+exception Identifier_err of string
 
 type identifier = (string * lambda) list
 
@@ -30,6 +33,15 @@ let lookforterm (l : char list) (i : identifier) : (lambda * char list) option =
 				aux xs m  
 	in aux l [] 
 
+let add_term (s : string) (t : lambda) (i : identifier) : identifier = 
+	let s_i = (List.map fst i) in 
+	if List.filter (String.starts_with ~prefix:s) s_i <> [] || List.exists (fun k -> String.starts_with ~prefix:k s) s_i 
+	then 
+		raise (Identifier_err ("Cannot add " ^ s ^" to the identifier. Another registered term starts with " ^ s ^ " or " ^ s ^ " starts with another term from the identifier, which might cause bugs."))
+	else
+		((s,t) :: i)
+
+
 let default_identifier = 
 	[
 	("If_then_else", L (V 1));
@@ -44,8 +56,7 @@ let default_identifier =
 	("Not",  L (A (A (V 1, L (L (V 2))), L (L (V 1)))));
 	("Succ", L (L (L (A (V 2, A (A (V 3, V 2), V 1))))));
 	("Add",L (L (L (L (A (V 3, A (A (V 4, V 2), V 1)))))));
-	("Mult", )
 	("Iszero",L (L (L (A (A (V 3, L (V 2)), V 2)))));
 	("Pred", L (L (L (A (A (A (V 3, L (L (A (V 1, A (V 2, V 4))))), L (V 2)), L (V 1))))));
-	("mult",  L (L (L (A (V 3, A (V 2, V 1))))));
+	("Mult",  L (L (L (A (V 3, A (V 2, V 1))))));
 	] 
