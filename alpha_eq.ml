13951e7d7a3e99279511538f4d17c55e20cc7b02
JeanGalte <wontusemailcom@protonmail.com>
2023-09-07 10:51:54 +0200

    remove old files

--- alpha_eq.ml
+++
@@ -1,4 +0,0 @@
-open Lambda
-open Parser
-
-let alpha_eq (l1 : string) (l2 : string) : bool = (parse_nature l1) = (parse_nature l2)
--- error_g.ml
+++
@@ -1,4 +0,0 @@
-let unwrap_ex (v : 'a option) (e : exn) : 'a = 
-	match v with
-	| None -> raise e
-	| Some x -> x
